80b26beeec646d1bff376ba0fd9e11cc5f30d6f2
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-14 12:55:22 +0300

    [FEEATURE] add router for catridge name

--- app/router/catridge.router.js
+++ app/router/catridge.router.js
@@ -3,7 +3,7 @@ import CatridgeController from '../contoller/catridge.controller.js';
 
 const router = new Express();
 
-router.get('/', (req, res) => {
+router.get('/info', (req, res) => {
     try {
         const data = new CatridgeController().getAll(req, res);
         res.send(data);
@@ -11,5 +11,14 @@ router.get('/', (req, res) => {
         res.send(`Не удается прочитать файл. Ошибка: ${e}`);
     }
 });
+
+router.get('/:catridgeName', (req, res) => {
+    try {
+        const data = new CatridgeController().getCatridge(req, res);
+        res.send(data);
+    } catch(e) {
+        res.send(`Не удается прочитать файл. Ошибка: ${e}`)
+    }
+})
 
 export default router;
