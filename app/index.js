9b70ea16b3484eaa0c9e17e274101e67d913f46b
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-15 14:18:40 +0300

    [FEATURE] update import path for router

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import express from "express";
-import catridgeRouter from './router/catridge.router.js';
+import getCatridgeRouter from './router/getCatridge.router.js';
 const app = express();
 
-app
-app.use('/catridge', catridgeRouter);
+app.use('/catridge', getCatridgeRouter);
 export default app;
