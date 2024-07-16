b1c46bef990e1f6899348808e7b47036f88f9368
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-16 09:35:30 +0300

    [FEATURE] add body-parser

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import getCatridgeRouter from './router/getCatridge.router.js';
 import postCatridgeRouter from './router/postCatridge.router.js';
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/catridge', getCatridgeRouter);
 app.use('/catridge', postCatridgeRouter);
