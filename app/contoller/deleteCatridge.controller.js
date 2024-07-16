77d25dd4523e28d3dde539db8cfa7c9ec2abd483
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-16 14:30:13 +0300

    [FEATURE] add router and controller for delete catridger from list

---
+++ app/contoller/deleteCatridge.controller.js
@@ -0,0 +1,24 @@
+import fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class DeleteCatridgeController {
+    constructor() {
+        this.pathToCatridgeData = path.join(__dirname, '..', 'db', 'catridgeData.json');
+        this.messageSuccesfull = "Изменения внесены успешно";
+    }
+
+    deleteCatridge(req, __res) {
+        const { catridgeName } = req.body;
+        const data = JSON.parse(fs.readFileSync(this.pathToCatridgeData, 'utf-8'));
+        delete data[catridgeName];
+        const newData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(this.pathToCatridgeData, newData);
+        return this.messageSuccesfull;
+    }
+}
+
+export default DeleteCatridgeController;
---
+++ app/router/deleteCatridge.router.js
@@ -0,0 +1,14 @@
+import Express from 'express';
+import DeleteCatridgeController from '../contoller/deleteCatridge.controller.js';
+
+const router = new Express();
+
+router.delete('/', (req, res) => {
+    try {
+        const data = new DeleteCatridgeController().deleteCatridge(req, res);
+        res.send(data)
+    } catch(e) {
+        res.send(`Не удалось удалить запись. Ошибка: ${e}`);
+    }
+})
+export default router;
