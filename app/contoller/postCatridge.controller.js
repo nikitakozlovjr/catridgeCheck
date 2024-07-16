d047554f665cfa5852e85518f94580a0a68f5825
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-16 11:42:25 +0300

    [FEATURE] add controller and router for feciept catridge count

--- app/contoller/postCatridge.controller.js
+++ app/contoller/postCatridge.controller.js
@@ -7,7 +7,9 @@ const __dirname = path.dirname(__filename);
 
 class PostCatridgeController {
     constructor() {
-        this.pathToCatridgeData = path.join(__dirname, '..', 'db', 'catridgeData.json')
+        this.pathToCatridgeData = path.join(__dirname, '..', 'db', 'catridgeData.json');
+        this.messageInvalidRemainder = "Недостаточно остатка на складе"
+        this.messageSuccesfull = "Изменения внесены успешно";
     }
 
     addCatridge(req, __res) {
@@ -18,6 +20,31 @@ class PostCatridgeController {
         const result = fs.writeFileSync(this.pathToCatridgeData, newData);
         return result;
     }
+
+    reflectIssue(req, __res) {
+        const data = JSON.parse(fs.readFileSync(this.pathToCatridgeData, 'utf-8'));
+        const { catridgeName, countIssued } = req.body;
+        if(data[catridgeName].count < countIssued) {
+            return this.messageInvalidRemainder;
+        }
+        const newCountStore = data[catridgeName].count - countIssued;
+        data[catridgeName].count = newCountStore;
+        const newData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(this.pathToCatridgeData, newData);
+        return this.messageSuccesfull;
+    }
+
+    reflectReceipt(req, __res) {
+        const data = JSON.parse(fs.readFileSync(this.pathToCatridgeData, 'utf-8'));
+        const { catridgeName, countIssued } = req.body;
+        const newCountStore = data[catridgeName].count + countIssued;
+        data[catridgeName].count = newCountStore;
+        const newData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(this.pathToCatridgeData, newData);
+        return this.messageSuccesfull;
+    }
+
+
 };
 
 export default PostCatridgeController;
--- app/router/postCatridge.router.js
+++ app/router/postCatridge.router.js
@@ -4,10 +4,27 @@ import PostCatridgeController from '../contoller/postCatridge.controller.js';
 const router = new Express();
 
 router.post('/', (req, res) => {
-    console.log(req.body);
     try {
         new PostCatridgeController().addCatridge(req, res);
         res.send("Изменения внесены успешно");
+    } catch(e) {
+        res.send(`Не удалось внести изменения ${e}`);
+    }
+});
+
+router.post('/issue', (req, res) => {
+    try {
+        const data = new PostCatridgeController().reflectIssue(req, res);
+        res.send(data)
+    } catch(e) {
+        res.send(`Не удалось внести изменения ${e}`);
+    }
+});
+
+router.post('/receipt', (req, res) => {
+    try {
+        const data = new PostCatridgeController().reflectReceipt(req, res);
+        res.send(data)
     } catch(e) {
         res.send(`Не удалось внести изменения ${e}`);
     }
