b59861e722e8bc47c9fe98b0a8a05c01a9561bde
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-16 13:49:23 +0300

    [FEATURE] add put router

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from 'body-parser';
 import getCatridgeRouter from './router/getCatridge.router.js';
 import postCatridgeRouter from './router/postCatridge.router.js';
+import putCatridgeRouter from './router/putCatridge.router.js';
 
 const app = express();
 
@@ -10,4 +11,5 @@ app.use(express.json());
 
 app.use('/catridge', getCatridgeRouter);
 app.use('/catridge', postCatridgeRouter);
+app.use('/catridge', putCatridgeRouter);
 export default app;
