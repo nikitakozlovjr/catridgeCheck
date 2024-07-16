80f89f2f6428c188c419fae635aa465d4500eeb1
nikitakozlovjr <nikita3726898@gmail.com>
2024-07-16 09:34:53 +0300

    [FEATURE] add router for catridge add

--- app/router/postCatridge.router.js
+++ app/router/postCatridge.router.js
@@ -4,9 +4,12 @@ import PostCatridgeController from '../contoller/postCatridge.controller.js';
 const router = new Express();
 
 router.post('/', (req, res) => {
+    console.log(req.body);
     try {
         new PostCatridgeController().addCatridge(req, res);
-        res.send();
+        res.send("Изменения внесены успешно");
+    } catch(e) {
+        res.send(`Не удалось внести изменения ${e}`);
     }
 });
 
